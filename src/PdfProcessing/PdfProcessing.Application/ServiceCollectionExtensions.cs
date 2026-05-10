e6d94281b5612377a99a03e26529aa23f85f4aeb
Sergey Voytovich <s.voytovich@outlook.com>
2026-05-10 20:22:31 +0100

    Implement DosumentsService

---
+++ src/PdfProcessing/PdfProcessing.Application/ServiceCollectionExtensions.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using PdfProcessing.Application.Services;
+
+namespace PdfProcessing.Application;
+
+public static class ServiceCollectionExtensions
+{
+    public static IServiceCollection AddPdfProcessingApplication(this IServiceCollection services)
+        => services
+            .AddScoped<IDocumentsService, DocumentsService>()
+        ;
+}
---
+++ src/PdfProcessing/PdfProcessing.Application/Services/DocumentsService.cs
@@ -0,0 +1,20 @@
+﻿using AutoMapper;
+using PdfProcessing.Api.Dtos;
+using PdfProcessing.Application.Data;
+using PdfProcessing.Domain;
+
+namespace PdfProcessing.Application.Services;
+
+internal class DocumentsService(IStorage storage, IMapper mapper) : IDocumentsService
+{
+    protected virtual IStorage Storage { get; } = storage;
+    protected virtual IMapper Mapper { get; } = mapper;
+
+
+
+    public async Task<IList<DocumentDto>> GetDocumentsAsync(CancellationToken cancellationToken = default)
+    {
+        var documents = await Storage.Documents.GetByStates([DocumentState.Received, DocumentState.Processing, DocumentState.Processed], cancellationToken);
+        return Mapper.Map<IList<DocumentDto>>(documents);
+    }
+}
