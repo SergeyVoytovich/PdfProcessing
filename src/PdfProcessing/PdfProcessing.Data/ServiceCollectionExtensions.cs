c6af2f70d3cfcffc24ee03c57f1367203181bd7d
Sergey Voytovich <s.voytovich@outlook.com>
2026-05-10 16:47:29 +0100

    Create extensions to add Data to DI

---
+++ src/PdfProcessing/PdfProcessing.Data/ServiceCollectionExtensions.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using PdfProcessing.Data.Entities;
+
+namespace PdfProcessing.Data;
+
+public static class ServiceCollectionExtensions
+{
+    public static IServiceCollection AddPdfProcessingData(this IServiceCollection services, Action<DbContextOptionsBuilder> options)
+        => services.AddDbContext<Context>(options);
+
+    public static IServiceCollection AddPdfProcessingDataToNpsql(this IServiceCollection services, string connectionString)
+        => services.AddPdfProcessingData(options => options.UseNpgsql(connectionString));
+}
