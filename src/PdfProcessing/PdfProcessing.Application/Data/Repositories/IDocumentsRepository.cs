05ff9c9c6e127a6be2502f26d4fc56d092b1ccce
Sergey Voytovich <s.voytovich@outlook.com>
2026-05-10 14:57:04 +0100

    Declare documents repository

---
+++ src/PdfProcessing/PdfProcessing.Application/Data/Repositories/IDocumentsRepository.cs
@@ -0,0 +1,13 @@
+﻿using PdfProcessing.Domain;
+
+namespace PdfProcessing.Application.Data.Repositories;
+
+public interface IDocumentsRepository
+{
+    Task<Document> GetByIdAsync(Guid Id);
+    Task<IList<Document>> GetAllAsync();
+
+    Task AddAsync(Document document);
+    Task UpdateAsync(Document document);
+    Task DeleteAsync(Guid Id);
+}
