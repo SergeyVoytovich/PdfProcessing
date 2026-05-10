d7e7e1271c50262c5b0bc94d9325aac7759659a9
Sergey Voytovich <s.voytovich@outlook.com>
2026-05-10 18:32:11 +0100

    Add repositories to DI

--- src/PdfProcessing/PdfProcessing.Data/ServiceCollectionExtensions.cs
+++ src/PdfProcessing/PdfProcessing.Data/ServiceCollectionExtensions.cs
@@ -1,13 +1,24 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
+using PdfProcessing.Application.Data.Repositories;
 using PdfProcessing.Data.Entities;
+using PdfProcessing.Data.Mapping;
+using PdfProcessing.Data.Repositories;
 
 namespace PdfProcessing.Data;
 
 public static class ServiceCollectionExtensions
 {
     public static IServiceCollection AddPdfProcessingData(this IServiceCollection services, Action<DbContextOptionsBuilder> options)
-        => services.AddDbContext<Context>(options);
+        => services.AddDbContext<Context>(options)
+                .AddScoped<IDocumentsRepository, DocumentsRepository>()
+                .AddScoped<IDocumentContentsRepository, DocumentContentsRepository>()
+                .AddAutoMapper(cnf => 
+                {
+                    cnf.AddProfile<GeneralProfile>();
+                    cnf.AddProfile<DocumentsProfile>();
+                })
+        ;
 
     public static IServiceCollection AddPdfProcessingDataToNpsql(this IServiceCollection services, string connectionString)
         => services.AddPdfProcessingData(options => options.UseNpgsql(connectionString));
