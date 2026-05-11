4d2e300cef0863c2147205e06d5ae377a8cd7077
Sergey Voytovich <s.voytovich@outlook.com>
2026-05-11 17:25:14 +0100

    Declare message bu

---
+++ src/PdfProcessing/PdfProcessing.Core/Messaging/Contracts/DocumentUploadedMessage.cs
@@ -0,0 +1,6 @@
+﻿namespace PdfProcessing.Messaging.Contracts;
+
+public record DocumentUploadedMessage
+{
+    public Guid DocumentId { get; set; }
+}
---
+++ src/PdfProcessing/PdfProcessing.Core/Messaging/IMessageBus.cs
@@ -0,0 +1,7 @@
+﻿namespace PdfProcessing.Messaging;
+
+internal interface IMessageBus
+{
+    Task PublishAsync<T>(T message, CancellationToken cancellation = default);
+    Task Subscribe<T>(Func<T, CancellationToken, Task> handler);
+}
