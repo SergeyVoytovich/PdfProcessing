ab5f2e52cc9ca061b6ea1578464fe8affb51ab38
Sergey Voytovich <s.voytovich@outlook.com>
2026-05-11 20:15:31 +0100

    Declare content extractor

--- src/PdfProcessing/PdfProcessing.Core/Messaging/Messages/IMessage.cs
+++ src/PdfProcessing/PdfProcessing.Core/Messaging/Messages/IMessage.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
-namespace PdfProcessing.Messaging.Messages;
+﻿namespace PdfProcessing.Messaging.Messages;
 
 public interface IMessage
 {
---
+++ src/PdfProcessing/PdfProcessing.Core/Utilities/IContentExtractor.cs
@@ -0,0 +1,8 @@
+﻿using PdfProcessing.Domain;
+
+namespace PdfProcessing.Utilities;
+
+public interface IContentExtractor
+{
+    public Task<IList<DocumentContent>> ExtractAscyn(string filePath, CancellationToken cancellationToken = default);
+}
