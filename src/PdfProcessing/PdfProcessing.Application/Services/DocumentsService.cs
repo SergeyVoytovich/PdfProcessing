e5af2b70e3fc999775767e41e2b8b45071ebad65
Sergey Voytovich <s.voytovich@outlook.com>
2026-05-11 14:45:41 +0100

    Implement addition of new document on

--- src/PdfProcessing/PdfProcessing.Application/Services/DocumentsService.cs
+++ src/PdfProcessing/PdfProcessing.Application/Services/DocumentsService.cs
@@ -10,7 +10,21 @@ internal class DocumentsService(IStorage storage, IMapper mapper) : IDocumentsSe
     protected virtual IStorage Storage { get; } = storage;
     protected virtual IMapper Mapper { get; } = mapper;
 
+    public async Task<DocumentDto> AddAscyn(string fileName, Stream stream, CancellationToken cancellationToken = default)
+    {
+        var path = await Storage.Files.AddAsync(fileName, stream, cancellationToken);
+        var document = new Document
+        {
+            Id = Guid.NewGuid(),
+            DisplayName = fileName,
+            FilePath = path,
+            State = DocumentState.Received
+        };
 
+        await Storage.Documents.AddAsync(document, cancellationToken);
+
+        return Mapper.Map<DocumentDto>(document);
+    }
 
     public async Task<IList<DocumentDto>> GetAsync(CancellationToken cancellationToken = default)
     {
--- src/PdfProcessing/PdfProcessing.Application/Services/IDocumentsService.cs
+++ src/PdfProcessing/PdfProcessing.Application/Services/IDocumentsService.cs
@@ -5,4 +5,5 @@ namespace PdfProcessing.Application.Services;
 public interface IDocumentsService
 {
     public Task<IList<DocumentDto>> GetAsync(CancellationToken cancellationToken = default);
+    public Task<DocumentDto> AddAscyn(string fileName, Stream stream, CancellationToken cancellationToken = default);
 }
