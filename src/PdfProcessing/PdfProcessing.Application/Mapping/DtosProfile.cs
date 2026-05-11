468c882a7101c6782e685bce88b2d8829c4494bd
Sergey Voytovich <s.voytovich@outlook.com>
2026-05-11 15:12:22 +0100

    Map document content

--- src/PdfProcessing/PdfProcessing.Application/Mapping/DtosProfile.cs
+++ src/PdfProcessing/PdfProcessing.Application/Mapping/DtosProfile.cs
@@ -1,5 +1,6 @@
 ﻿using AutoMapper;
 using PdfProcessing.Api.Dtos;
+using PdfProcessing.Application.Dtos;
 using PdfProcessing.Domain;
 
 namespace PdfProcessing.Application.Mapping;
@@ -12,6 +13,15 @@ internal class DtosProfile : Profile
             .ForMember(dst => dst.Id, opt => opt.MapFrom(src => src.Id))
             .ForMember(dst => dst.Name, opt => opt.MapFrom(src => src.DisplayName))
             .ForMember(dst => dst.State, opt => opt.MapFrom(src => src.State.ToString()))
+            ;
+
+        CreateMap<Document, DocumentContentDto>()
+            .IncludeBase<Document, DocumentDto>()
+            ;
+
+        CreateMap<DocumentContent, PageContentDto>()
+            //.ForMember(dst => dst.PageNumber, opt => opt.MapFrom(src => src.PageNumber))
+            .ForMember(dst => dst.TextContent, opt => opt.MapFrom(src => src.Content))
             ;
     }
 }
