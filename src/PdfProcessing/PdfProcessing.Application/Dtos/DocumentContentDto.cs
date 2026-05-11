30ab7d6e14c2c14b95a21308522ecaadfbdc8a76
Sergey Voytovich <s.voytovich@outlook.com>
2026-05-11 15:08:17 +0100

    Declare document content

---
+++ src/PdfProcessing/PdfProcessing.Application/Dtos/DocumentContentDto.cs
@@ -0,0 +1,10 @@
+﻿using PdfProcessing.Api.Dtos;
+using System.Text.Json.Serialization;
+
+namespace PdfProcessing.Application.Dtos;
+
+public record DocumentContentDto : DocumentDto
+{
+    [JsonPropertyName("contents")]
+    public IList<PageContentDto> Pages { get; set; } = [];
+}
---
+++ src/PdfProcessing/PdfProcessing.Application/Dtos/PageContentDto.cs
@@ -0,0 +1,12 @@
+﻿using System.Text.Json.Serialization;
+
+namespace PdfProcessing.Application.Dtos;
+
+public record PageContentDto
+{
+    [JsonPropertyName("page")]
+    public int PageNumber { get; set; }
+
+    [JsonPropertyName("content")]
+    public string TextContent { get; set; } = string.Empty;
+}
